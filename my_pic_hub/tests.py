1f777680b11727e026c9d7c772d49b1d64f37429
asandelarvine <asandelarvine@gmail.com>
2021-11-28 14:22:49 +0300

    added a test for save images

--- my_pic_hub/tests.py
+++ my_pic_hub/tests.py
@@ -7,3 +7,12 @@ class ImageTestClass(TestCase):
     # Set up method
     def setUp(self):
         self.image= Image(image = 'imageurl', name ='test_image', image_description ='image test description')
+
+    # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.image, Image))
+
+    def test_save_images(self):
+        self.image.save_images()
+        images = Image.objects.all()
+        self.assertTrue(len(images)>0)
