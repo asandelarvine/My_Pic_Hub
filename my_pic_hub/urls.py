065662b54aa196a1c6deede81b2e0d9cc9188593
asandelarvine <asandelarvine@gmail.com>
2021-11-28 13:46:58 +0300

    created a list named urlpatterns which will be a list of url instances for the app then called the  url function.

---
+++ my_pic_hub/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path, re_path, include
+from django.conf.urls.static import static
+from django.conf import settings
+from . import views
+
+urlpatterns=[
+    re_path('^$',views.home,name = 'home'),
+    re_path(r'^all/',views.index, name= 'all'),
+    re_path(r'^categories/(\d+)',views.categories,name = 'categories'),
+    re_path(r'^details/(\d+)',views.details,name = 'details'),
+    re_path(r'^search/',views.search, name= 'search')            
+
+    
+]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+
+ 
